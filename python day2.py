d46bc9f68a7e101ae8b0f9fab61f2760c33473d6
kondaniveditha2008 <kondaniveditha9@gmail.com>
2026-07-29 22:54:19 +0530

    Add files via upload

---
+++ python day2.py
@@ -0,0 +1,37 @@
+
+
+
+name = "Sri Niveditha"
+age = 20
+
+print(name)
+print(age)
+
+
+print(type(name))
+print(type(age))
+
+
+
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+
+
+marks = 80
+
+if marks >= 50:
+    print("Pass")
+else:
+    print("Fail")
+
+
+for i in range(1, 6):
+    print(i)
+
+def greet(name):
+    print("Hello", name)
+
+greet("Sri")
